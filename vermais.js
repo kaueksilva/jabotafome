a0d4e90ff8e6ba604c7e2e001243400991d9b788
Kauê Silva <kaueksilva12@gmail.com>
2023-09-15 12:31:09 -0300

    commit

---
+++ vermais.js
@@ -0,0 +1,11 @@
+// Função para abrir o popup
+function openPopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "block";
+}
+
+// Função para fechar o popup
+function closePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+}
